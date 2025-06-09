1e8258e83d389e7fa28067d31374351dddc0c8f5
Daniil Riazanov <ryazanow45@gmail.com>
2025-06-09 10:29:57 +0200

    uploads

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import fs from "fs";
 import multer from "multer";
 import mongoose from "mongoose";
 import {
@@ -24,6 +25,9 @@ const app = express();
 
 const storage = multer.diskStorage({
   destination: (_, __, cb) => {
+    if(!fs.existsSync('uploads')){
+      fs.mkdirSync('uploads')
+    }
     cb(null, "uploads");
   },
   filename: (_, file, cb) => {
